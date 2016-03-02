a56dae1fd004bd60185477d4c450bc6fdd1876d9
Benjamin Huang <coolconfucius@yahoo.com>
2016-03-01 18:05:50 -0800

    update backend

--- routes/items.js
+++ routes/items.js
@@ -3,8 +3,8 @@
 var express = require('express');
 var router = express.Router();
 
-var Item = reqiure('../models/item'); 
-var User = reqiure('../models/user'); 
+var Item = require('../models/item'); 
+var User = require('../models/user'); 
 
 
 router.get('/', function(req, res, next) {
@@ -14,7 +14,9 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:userid', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Item.getUserItems(req.params.userid, function(err, userItems) {
+    res.status(err ? 400:200).send(err||userItems);
+  });
 });
 
 router.post('/', User.isAuthenticated, function(req, res, next) {
