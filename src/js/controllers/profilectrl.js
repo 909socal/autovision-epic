3d98f1e0dc06da4c65804d1b3c8ad1786ad3f127
Benjamin Huang <coolconfucius@yahoo.com>
2016-03-03 11:46:24 -0800

    worked on edit form

--- src/js/controllers/profilectrl.js
+++ src/js/controllers/profilectrl.js
@@ -6,7 +6,7 @@ app.controller('profileCtrl', function($scope, $rootScope, $state, Item) {
  Item.getAllItems()
       .then(function(res){
         $scope.items = res.data; 
-        $scope.category=$state.params.type;
+        $scope.category = $state.params.type;
         console.log('Hi', $scope.items);
       });
 
@@ -17,26 +17,31 @@ app.controller('profileCtrl', function($scope, $rootScope, $state, Item) {
   }
 
   $scope.isEditing = false; 
+  $scope.editItem = {}; 
   $scope.editId; 
 
   $scope.edit = function(item){
+    console.log("scope edit");
     var itemId = item._id.toString();
     if ($scope.editId === itemId) {
       $scope.isEditing = false; 
       $scope.editId = '';       
+      $scope.editItem = {};       
     } else {
       $scope.isEditing = true; 
       $scope.editId = itemId;
+      $scope.editItem = item;
     }
     
-    var realIndex = $scope.items.indexOf(item); 
+    // var realIndex = $scope.items.indexOf(item); 
     // $scope.items[realIndex].iscomplete = !$scope.items[realIndex].iscomplete;
 
     // Item.edit(item._id.toString(), $scope.editItem); 
   }
 
-  $scope.editConfirm = function(itemId){
-    Item.edit(itemId, $scope.editItem);  
-  }
+  // $scope.editConfirm = function(itemId){
+  //   var realIndex = $scope.items.indexOf(item); 
+  //   Item.edit(itemId, $scope.editItem);  
+  // }
   
 });
