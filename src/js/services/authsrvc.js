0767fe5b667e4379bac177fd3a66e6e87b66a19d
Benjamin Huang <coolconfucius@yahoo.com>
2016-03-05 21:53:22 -0800

    cleaned auth service

--- src/js/services/authsrvc.js
+++ src/js/services/authsrvc.js
@@ -8,17 +8,6 @@ app.service('Auth', function($http, $state, $localStorage, $rootScope) {
 
   this.login = (user) => {
     return $http({method: 'POST', url: '/users/login', data: user});
-    // $http({method: 'POST', url: '/users/login', data: user});
-    // .then((data)=>{
-    //   this.token = data;
-    //   $localStorage.token = this.token;
-    //   $state.go('profile');
-    //   // return data;
-    //   return true;
-    // },
-    // function err(err) {
-    //   console.log('inside err', err);
-    // });    
   }
   
   this.logout = () => {
@@ -27,36 +16,8 @@ app.service('Auth', function($http, $state, $localStorage, $rootScope) {
     $state.go('home');
   }
 
-  // this.user = () => {
-  //   // return $localStorage.token; 
-  //   // return $http({method: 'GET', url: '/users'})
-  //   // .then((data)=>{
-  //   //   if (data) {
-  //   //     this.token = data;
-  //   //     $localStorage.token = this.token;
-  //   //   };
-  //   // },
-  //   // function err(err) {
-  //   //   console.log('err', err);
-  //   // });    
-  //   // return $http.get('/users');
-  //   // .then(res => {
-  //   //   this.data = res; 
-  //   //   $localStorage.token = res;
-  //   // });  
-
-  //   console.log("token HERE??? \n", $localStorage.token);
-  //   return $localStorage.token; 
-  // }
-
-  // this.user = function(cb){
-  //   cb($localStorage.token); 
-  // }
 
   this.user = function() {
-    // return $http.get('/users').then(res => {
-    //   this.data = res.data; 
-    // });  
     this.data = $localStorage.token; 
     console.log($localStorage.token, "LOCALSTORAGE TOKEN \n");
     $rootScope.user = $localStorage.token; 
