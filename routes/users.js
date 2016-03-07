c7da0a8ecac9294fcd44c1efa366e0cf2b21ec33
Benjamin Huang <coolconfucius@yahoo.com>
2016-03-07 14:31:45 -0800

    now uses sweetalert

--- routes/users.js
+++ routes/users.js
@@ -23,6 +23,9 @@ router.post('/register', function(req, res, next){
 
 router.post('/login', function(req, res, next){
   User.authenticate(req.body, function(err, user){
+    // if (err) return res.send(err);
+    if (err) return res.status(401).send(err);
+    console.log("Hitting token");
     var token = user.token(); 
     res.cookie('token', token).send(token);
   }); 
