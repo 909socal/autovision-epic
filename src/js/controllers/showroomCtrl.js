c23e2b17717cd2d259957e8c2ef728af349c23e5
Gurjot Singh <celica09@gmail.com>
2016-03-11 15:08:49 -0800

    fix sizing

--- src/js/controllers/showroomCtrl.js
+++ src/js/controllers/showroomCtrl.js
@@ -43,18 +43,18 @@ app.controller('showroomCtrl', function($scope, $rootScope, $localStorage, Autof
     	{
 				imgSrc: 'http://www.m5board.com/vbulletin/attachment.php?attachmentid=96957&stc=1&thumb=1&d=1262574914',
 				paragraph: {
-					model: 'Model: Nissan',
-					make: 'Make: Skyline',
-					user: 'UserName: laugh_drive'
+					model: 'Model: BMW',
+					make: 'Make: m5',
+					user: 'UserName: AutoVision'
 				}
 			},
 			{
 		
 				imgSrc: 'http://images.cdn.stuff.tv/sites/stuff.tv/files/Mercedes-AMG-GT-front.JPG',
 				paragraph: {
-					model: 'Model: Nissan',
-					make: 'Make: Skyline',
-					user: 'UserName: laugh_drive'
+					model: 'Model: Mercedes',
+					make: 'Make: AMG GT',
+					user: 'UserName: AutoVision'
 				}
 			},
 			{
@@ -74,26 +74,26 @@ app.controller('showroomCtrl', function($scope, $rootScope, $localStorage, Autof
 		{
 			imgSrc: 'http://www.m5board.com/vbulletin/attachment.php?attachmentid=96957&stc=1&thumb=1&d=1262574914',
 			paragraph: {
-				model: 'Model: Nissan',
-				make: 'Make: Skyline',
-				user: 'UserName: laugh_drive'
+			    model: 'Model: BMW',
+					make: 'Make: m5',
+					user: 'UserName: AutoVision'
 			}
 		},
 		{
 	
 			imgSrc: 'http://images.cdn.stuff.tv/sites/stuff.tv/files/Mercedes-AMG-GT-front.JPG',
 			paragraph: {
-				model: 'Model: Nissan',
-				make: 'Make: Skyline',
-				user: 'UserName: laugh_drive'
+				  model: 'Model: Mercedes',
+					make: 'Make: AMG GT',
+					user: 'UserName: AutoVision'
 			}
 		},
 		{
 			imgSrc: 'http://media.caranddriver.com/images/media/51/2013-maserati-granturismo-sport-inline-2-photo-482273-s-original.jpg',
 			paragraph: {
-				model: 'Model: Nissan',
-				make: 'Make: Skyline',
-				user: 'UserName: laugh_drive'
+				model: 'Model: Maserati',
+				make: 'Make: GranTurismo Sport',
+				user: 'UserName: AutoVision'
 			}
 		},
 		{
@@ -101,95 +101,95 @@ app.controller('showroomCtrl', function($scope, $rootScope, $localStorage, Autof
 			paragraph: {
 				model: 'Model: Nissan',
 				make: 'Make: Skyline',
-				user: 'UserName: laugh_drive'
+				user: 'UserName: AutoVision'
 			}
 		},
 		{
 			imgSrc: 'http://www.mrwallpaper.com/wallpapers/toyota-supra-twin-turbo.jpg',
 			paragraph: {
-				model: 'Model: Nissan',
-				make: 'Make: Skyline',
-				user: 'UserName: laugh_drive'
+				model: 'Model: Toyota',
+				make: 'Make: Supra',
+				user: 'UserName: AutoVision'
 			}
 		},
 		{
 			imgSrc: 'http://farm8.staticflickr.com/7083/7185053970_bc652a611c_o.jpg',
 			paragraph: {
-				model: 'Model: Nissan',
-				make: 'Make: Skyline',
-				user: 'UserName: laugh_drive'
+				model: 'Model: BMW',
+				make: 'Make: M3',
+				user: 'UserName: AutoVision'
 			}
 		},
 		{
 			imgSrc: 'http://bestcarmag.com/sites/default/files/16997027038975-yellow-maserati-granturismo.jpg',
 			paragraph: {
-				model: 'Model: Nissan',
-				make: 'Make: Skyline',
-				user: 'UserName: laugh_drive'
+				model: 'Model: Maserati',
+				make: 'Make: GranTurismo',
+				user: 'UserName: AutoVision'
 			}
 		},
 		{
 			imgSrc: 'http://s1.cdn.autoevolution.com/images/news/brabus-b63-620-widestar-g-class-spotted-in-dubai-video-68273_1.png',
 			paragraph: {
-				model: 'Model: Nissan',
-				make: 'Make: Skyline',
-				user: 'UserName: laugh_drive'
+				model: 'Model: Mercedes',
+				make: 'Make: G Class',
+				user: 'UserName: AutoVision'
 			}
 		},
 		{
 			imgSrc: 'http://www.thesupercars.org/wp-content/uploads/2010/01/2010-speedART-PS9-650-Porsche-Panamera-Turbo-Side-.jpg',
 			paragraph: {
-				model: 'Model: Nissan',
-				make: 'Make: Skyline',
-				user: 'UserName: laugh_drive'
+				model: 'Model: Porsche',
+				make: 'Make: Panamera',
+				user: 'UserName: AutoVision'
 			}
 		},
 		{
 			imgSrc: 'http://i553.photobucket.com/albums/jj386/Jags86/ratrod.jpg',
 			paragraph: {
-				model: 'Model: Nissan',
-				make: 'Make: Skyline',
-				user: 'UserName: laugh_drive'
+				model: 'Model: Ford',
+				make: 'Make: Model A',
+				user: 'UserName: AutoVision'
 			}
 		},
 		{
 			imgSrc: 'http://k-break.com/platinum/30CELSIOR_S/image.jpg',
 			paragraph: {
-				model: 'Model: Nissan',
-				make: 'Make: Skyline',
-				user: 'UserName: laugh_drive'
+				model: 'Model: Lexus',
+				make: 'Make: LS400',
+				user: 'UserName: AutoVision'
 			}
 		},
 		{
 			imgSrc: 'http://g02.a.alicdn.com/kf/HTB1OIMIHVXXXXbVXFXXq6xXFXXXG/Vintage-Rerto-font-b-Car-b-font-Cadillac-De-Ville-font-b-Lowrider-b-font-42.jpg',
 			paragraph: {
-				model: 'Model: Nissan',
-				make: 'Make: Skyline',
-				user: 'UserName: laugh_drive'
+				model: 'Model: Cadillac',
+				make: 'Make: De Ville',
+				user: 'UserName: AutoVision'
 			}
 		},
 		{
 			imgSrc: 'http://www.addictivedesertdesigns.com/images/products/secondary/f012892450103/f012892450103--4/ford-raptor-bumpers.jpg',
 			paragraph: {
-				model: 'Model: Nissan',
-				make: 'Make: Skyline',
-				user: 'UserName: laugh_drive'
+				model: 'Model: Ford',
+				make: 'Make: Raptor',
+				user: 'UserName: AutoVision'
 			}
 		},
 		{
 			imgSrc: 'http://fullhdpictures.com/wp-content/uploads/2016/02/Amazing-Porsche-Cayenne-Turbo-Wallpaper.jpeg',
 			paragraph: {
-				model: 'Model: Nissan',
-				make: 'Make: Skyline',
-				user: 'UserName: laugh_drive'
+				model: 'Model: Porsche',
+				make: 'Make: Cayenne Turbo',
+				user: 'UserName: AutoVision'
 			}
 		},
 		{
 			imgSrc: 'http://www.wallpaperup.com/uploads/wallpapers/2013/07/01/112231/big_thumb_1d6e26be14c772f5e53a189f6708c59e.jpg',
 			paragraph: {
-				model: 'Model: Nissan',
-				make: 'Make: Skyline',
-				user: 'UserName: laugh_drive'
+				model: 'Model: Jeep',
+				make: 'Make: Wrangler',
+				user: 'UserName: AutoVision'
 			}
 		}	
 	]	
