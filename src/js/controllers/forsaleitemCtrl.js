329a99e687759e261df1847aa0350b551a9b9a46
Benjamin Huang <coolconfucius@yahoo.com>
2016-03-11 22:26:47 -0800

    clenaed forsaleitemCtrl

--- src/js/controllers/forsaleitemCtrl.js
+++ src/js/controllers/forsaleitemCtrl.js
@@ -1,14 +1,12 @@
 var app = angular.module('app');
 
 app.controller('forsaleitemCtrl', function($scope, $rootScope, $state, Item, Auth, $localStorage) {
-  // console.log('in forsaleitemsCtrl', $state.params)
   $rootScope.user = $localStorage.token; 
 
   Item.getAllItems()
   .then(function(res){
     $scope.items = res.data; 
     $scope.category=$state.params.type;
-        // console.log('Hi', $scope.items);
   });
 
   $scope.goToShowroom = function() {
