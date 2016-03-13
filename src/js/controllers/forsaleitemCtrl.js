cd3b1959db183d7889c50f9a3ff7503d85f102a2
Michele Degges <mdeggies@gmail.com>
2016-03-12 23:14:20 -0800

    fixed bugs on forsale page

--- src/js/controllers/forsaleitemCtrl.js
+++ src/js/controllers/forsaleitemCtrl.js
@@ -1,14 +1,14 @@
 var app = angular.module('app');
 
 app.controller('forsaleitemCtrl', function($scope, $rootScope, $state, Item, Auth, $localStorage) {
-  $rootScope.user = $localStorage.token; 
+  $rootScope.user = $localStorage.token;
 
   Item.getAllItems()
   .then(function(res){
-    $scope.items = res.data; 
+    $scope.items = res.data;
     var title = $state.params.type;
     $scope.category = title;
-    console.log('category:', $scope.category);
+    //console.log('category:', $scope.category);
     $scope.title = title.charAt(0).toUpperCase() + title.slice(1);
   });
 
