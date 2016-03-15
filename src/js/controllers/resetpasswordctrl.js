8512f9194edf0ee783e1a8a6d6356490dd194ed1
valmsmith39a <valmsmith39a@gmail.com>
2016-03-14 17:20:32 -0700

    Implemented forgot password

---
+++ src/js/controllers/resetpasswordctrl.js
@@ -0,0 +1,7 @@
+app.controller('resetPasswordCtrl', function($scope, $state, Auth, $localStorage, $rootScope) {
+
+  $scope.resetPassword = function() {
+    console.log('resetpasswordctrl');
+
+  };
+});
